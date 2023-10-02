31b4cf9d3681f4fee4c03edf3697be2649316376
terence <terenceshawn18@outlook.com>
2023-10-02 08:11:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let companyname = "Scuber";
+const companyName = "Scuber"
+
+const mostprofitableNeighborhood = "Chelsea";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+const  companyceo = "Susan Smith";
+
+let companyCeo = "Susan Smith";
